49b6d77049610ebe6adb157da2f75cfe0784e97d
kavyasrini24 <103719161+kavyasrini24@users.noreply.github.com>
2022-04-28 09:19:23 +0530

    Create assignment.py

---
+++ assignment.py
@@ -0,0 +1,8 @@
+def personal_details():
+    name = input("enter the name:")
+    age = input("enter the age:")
+    address = input("enter the address:")
+    print("Name: {}\nAge: {}\nAddress: {}".format(name, age, address))
+
+
+personal_details()
