34d67506667f8c1b1b7a39362cdad85f5cc6569e
kavyasrini24 <103719161+kavyasrini24@users.noreply.github.com>
2024-07-25 18:13:45 +0530

    Update Test parser.py

--- Test parser.py
+++ Test parser.py
@@ -1,17 +1,17 @@
 import unittest
 from src.parser import parse_properties_file
+from src.config import load_xml_files
 
 class TestParser(unittest.TestCase):
+
     def setUp(self):
-        # Define properties for testing
-        self.properties = {
-            'key1': '${value1',
-            'key2': 'value2'
-        }
+        self.xml_files = load_xml_files('src/test/test_data')
 
     def test_parse_properties_file(self):
-        problems = parse_properties_file(self.properties)
-        self.assertGreater(len(problems), 0)
+        properties = {'key1': '${reference1}', 'key2': 'value2'}
+        problems, missing_properties = parse_properties_file(properties, self.xml_files)
+        self.assertIn('Key \'key1\' references non-existent XML value \'reference1\'', problems)
+        self.assertEqual(missing_properties['key1'], '${reference1}')
 
 if __name__ == '__main__':
     unittest.main()
