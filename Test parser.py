7ab2b3a4a791545c7e8f27ef84b1bdc237511eac
kavyasrini24 <103719161+kavyasrini24@users.noreply.github.com>
2024-07-25 20:11:41 +0530

    Update Test parser.py

--- Test parser.py
+++ Test parser.py
@@ -1,17 +1,19 @@
 import unittest
 from src.parser import parse_properties_file
+from src.model import Model
 
 class TestParser(unittest.TestCase):
     def setUp(self):
-        # Define properties for testing
+        self.model = Model('src/test/test_data/')
         self.properties = {
-            'key1': '${value1',
-            'key2': 'value2'
+            'key1': 'value1'
         }
 
     def test_parse_properties_file(self):
-        problems = parse_properties_file(self.properties)
-        self.assertGreater(len(problems), 0)
+        problems, missing_properties, new_properties = parse_properties_file(self.properties, self.model.xml_files)
+        self.assertEqual(len(problems), 0)
+        self.assertEqual(len(missing_properties), 0)
+        self.assertEqual(len(new_properties), 0)
 
 if __name__ == '__main__':
     unittest.main()
