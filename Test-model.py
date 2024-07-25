6b94633417bf8232b39cc35017f84a7bd7d38377
kavyasrini24 <103719161+kavyasrini24@users.noreply.github.com>
2024-07-25 13:43:01 +0530

    Update Test-model.py

--- Test-model.py
+++ Test-model.py
@@ -2,9 +2,15 @@ import unittest
 from src.model import get_key_value_pairs
 
 class TestModel(unittest.TestCase):
+    def setUp(self):
+        # Define properties for testing
+        self.properties = {
+            'key1': 'value1',
+            'key2': 'value2'
+        }
+
     def test_get_key_value_pairs(self):
-        properties = {'key1': 'value1', 'key2': 'value2'}
-        key_value_pairs = get_key_value_pairs(properties)
+        key_value_pairs = get_key_value_pairs(self.properties)
         self.assertEqual(key_value_pairs['key1'], 'value1')
         self.assertEqual(key_value_pairs['key2'], 'value2')
 
