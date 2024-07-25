f255800d1d2a84a87c33f3c0b7251b773f467816
kavyasrini24 <103719161+kavyasrini24@users.noreply.github.com>
2024-07-25 19:53:33 +0530

    Update Model.py

--- Model.py
+++ Model.py
@@ -2,26 +2,13 @@ import os
 import xml.etree.ElementTree as ET
 
 class Model:
-    def __init__(self, application_properties_file, xml_files_directory):
-        self.application_properties_file = application_properties_file
-        self.xml_files_directory = xml_files_directory
-        self.xml_files = self.load_xml_files()
+    def __init__(self, xml_files_directory):
+        self.xml_files = self.load_xml_files(xml_files_directory)
 
-    def load_xml_files(self):
+    def load_xml_files(self, directory):
         xml_files = {}
-        for filename in os.listdir(self.xml_files_directory):
-            if filename.endswith(".xml"):
-                file_path = os.path.join(self.xml_files_directory, filename)
-                try:
-                    tree = ET.parse(file_path)
-                    xml_files[filename] = tree.getroot()
-                except ET.ParseError:
-                    print(f"Error parsing XML file: {file_path}")
+        for filename in os.listdir(directory):
+            if filename.endswith('.xml'):
+                file_path = os.path.join(directory, filename)
+                xml_files[filename] = ET.parse(file_path).getroot()
         return xml_files
-
-    def get_value_from_xml(self, key):
-        for filename, root in self.xml_files.items():
-            for elem in root.iter():
-                if elem.tag == key:
-                    return elem.text
-        return None
