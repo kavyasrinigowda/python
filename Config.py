0f4bb113b6118bd2b89c94ede11083e47e178a4d
kavyasrini24 <103719161+kavyasrini24@users.noreply.github.com>
2024-07-25 19:47:44 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,8 +1,19 @@
-def read_properties_file(filepath):
+import os
+import xml.etree.ElementTree as ET
+
+def read_properties_file(file_path):
     properties = {}
-    with open(filepath, 'r') as file:
+    with open(file_path, 'r') as file:
         for line in file:
             if '=' in line:
                 key, value = line.strip().split('=', 1)
-                properties[key.strip()] = value.strip()
+                properties[key] = value
     return properties
+
+def load_xml_files(directory):
+    xml_files = {}
+    for filename in os.listdir(directory):
+        if filename.endswith('.xml'):
+            file_path = os.path.join(directory, filename)
+            xml_files[filename] = ET.parse(file_path).getroot()
+    return xml_files
