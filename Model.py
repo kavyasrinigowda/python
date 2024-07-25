344f109b9bdb39e77f79f340c011ab3621e0679e
kavyasrini24 <103719161+kavyasrini24@users.noreply.github.com>
2024-07-25 20:08:13 +0530

    Update Model.py

--- Model.py
+++ Model.py
@@ -1,24 +1,18 @@
 import os
-from xml.etree import ElementTree as ET
+import xml.etree.ElementTree as ET
 
 class Model:
-    def __init__(self, properties_file, xml_files_directory):
-        self.properties = self.load_properties(properties_file)
+    def __init__(self, xml_files_directory):
         self.xml_files = self.load_xml_files(xml_files_directory)
-    
-    def load_properties(self, file_path):
-        properties = {}
-        with open(file_path, 'r') as file:
-            for line in file:
-                if '=' in line and line.strip() and not line.startswith('#'):
-                    key, value = line.strip().split('=', 1)
-                    properties[key] = value
-        return properties
-    
+
     def load_xml_files(self, directory):
         xml_files = {}
-        for file_name in os.listdir(directory):
-            if file_name.endswith('.xml'):
-                file_path = os.path.join(directory, file_name)
-                xml_files[file_name] = ET.parse(file_path).getroot()
+        for filename in os.listdir(directory):
+            if filename.endswith('.xml'):
+                file_path = os.path.join(directory, filename)
+                try:
+                    tree = ET.parse(file_path)
+                    xml_files[filename] = tree.getroot()
+                except ET.ParseError as e:
+                    print(f"Error parsing {filename}: {e}")
         return xml_files
