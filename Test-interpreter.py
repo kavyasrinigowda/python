28f4e690758ef264269ca88b5dc077457776490f
kavyasrini24 <103719161+kavyasrini24@users.noreply.github.com>
2024-07-25 13:40:44 +0530

    Update Test-interpreter.py

--- Test-interpreter.py
+++ Test-interpreter.py
@@ -1,19 +1,25 @@
 import unittest
 from xml.etree import ElementTree as ET
 from src.interpreter import load_xml_files, find_differences_in_xml
+import os
 
 class TestInterpreter(unittest.TestCase):
     def setUp(self):
+        # Define the test XML file paths
+        self.test_data_dir = 'src/test/test_data'
         self.xml_files = {
-            'src/test/test_data/cbc-osb-common.xml': ET.ElementTree(ET.Element("root")).getroot(),
-            'src/test/test_data/cbc-osb-dev-tire.xml': ET.ElementTree(ET.Element("root")).getroot(),
-            'src/test/test_data/cbc-osb-dev01.xml': ET.ElementTree(ET.Element("root")).getroot(),
-            'src/test/test_data/cbc-service-cbc-osb-common.xml': ET.ElementTree(ET.Element("root")).getroot(),
-            'src/test/test_data/cbc-service-cbc-osb-dev-tire.xml': ET.ElementTree(ET.Element("root")).getroot(),
-            'src/test/test_data/cbc-service-cbc-osb-dev01.xml': ET.ElementTree(ET.Element("root")).getroot()
+            'cbc-osb-common.xml': os.path.join(self.test_data_dir, 'cbc-osb-common.xml'),
+            'cbc-osb-dev-tire.xml': os.path.join(self.test_data_dir, 'cbc-osb-dev-tire.xml'),
+            'cbc-osb-dev01.xml': os.path.join(self.test_data_dir, 'cbc-osb-dev01.xml'),
+            'cbc-service-cbc-osb-common.xml': os.path.join(self.test_data_dir, 'cbc-service-cbc-osb-common.xml'),
+            'cbc-service-cbc-osb-dev-tire.xml': os.path.join(self.test_data_dir, 'cbc-service-cbc-osb-dev-tire.xml'),
+            'cbc-service-cbc-osb-dev01.xml': os.path.join(self.test_data_dir, 'cbc-service-cbc-osb-dev01.xml')
         }
-        for file in self.xml_files:
-            root = self.xml_files[file]
+        # Load the XML files
+        self.xml_files_content = load_xml_files(self.xml_files.values())
+        
+        # Prepare mock XML content
+        for path, root in self.xml_files_content.items():
             child = ET.SubElement(root, "item")
             name = ET.SubElement(child, "name")
             name.text = "key"
@@ -22,7 +28,7 @@ class TestInterpreter(unittest.TestCase):
 
     def test_find_differences_in_xml(self):
         key = 'key'
-        differences = find_differences_in_xml(self.xml_files, key)
+        differences = find_differences_in_xml(self.xml_files_content, key)
         self.assertGreater(len(differences), 0)
 
 if __name__ == '__main__':
