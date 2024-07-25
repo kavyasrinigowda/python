8472171a269629018c3d3c3e74faa28eb016e54e
kavyasrini24 <103719161+kavyasrini24@users.noreply.github.com>
2024-07-25 20:10:54 +0530

    Update Test-interpreter.py

--- Test-interpreter.py
+++ Test-interpreter.py
@@ -1,35 +1,19 @@
 import unittest
-from xml.etree import ElementTree as ET
-from src.interpreter import load_xml_files, find_differences_in_xml
-import os
+from src.parser import parse_properties_file
+from src.model import Model
 
-class TestInterpreter(unittest.TestCase):
+class TestParser(unittest.TestCase):
     def setUp(self):
-        # Define the test XML file paths
-        self.test_data_dir = 'src/test/test_data'
-        self.xml_files = {
-            'cbc-osb-common.xml': os.path.join(self.test_data_dir, 'cbc-osb-common.xml'),
-            'cbc-osb-dev-tire.xml': os.path.join(self.test_data_dir, 'cbc-osb-dev-tire.xml'),
-            'cbc-osb-dev01.xml': os.path.join(self.test_data_dir, 'cbc-osb-dev01.xml'),
-            'cbc-service-cbc-osb-common.xml': os.path.join(self.test_data_dir, 'cbc-service-cbc-osb-common.xml'),
-            'cbc-service-cbc-osb-dev-tire.xml': os.path.join(self.test_data_dir, 'cbc-service-cbc-osb-dev-tire.xml'),
-            'cbc-service-cbc-osb-dev01.xml': os.path.join(self.test_data_dir, 'cbc-service-cbc-osb-dev01.xml')
+        self.model = Model('src/test/test_data/')
+        self.properties = {
+            'key1': 'value1'
         }
-        # Load the XML files
-        self.xml_files_content = load_xml_files(self.xml_files.values())
-        
-        # Prepare mock XML content
-        for path, root in self.xml_files_content.items():
-            child = ET.SubElement(root, "item")
-            name = ET.SubElement(child, "name")
-            name.text = "key"
-            value = ET.SubElement(child, "value")
-            value.text = "value"
 
-    def test_find_differences_in_xml(self):
-        key = 'key'
-        differences = find_differences_in_xml(self.xml_files_content, key)
-        self.assertGreater(len(differences), 0)
+    def test_parse_properties_file(self):
+        problems, missing_properties, new_properties = parse_properties_file(self.properties, self.model.xml_files)
+        self.assertEqual(len(problems), 0)
+        self.assertEqual(len(missing_properties), 0)
+        self.assertEqual(len(new_properties), 0)
 
 if __name__ == '__main__':
     unittest.main()
