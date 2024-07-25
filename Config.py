e97f061a43c78c79a140fe6b7f8feb9015b1b955
kavyasrini24 <103719161+kavyasrini24@users.noreply.github.com>
2024-07-25 19:36:04 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,18 +1,8 @@
-import os
-
-def read_properties_file(file_path):
+def read_properties_file(filepath):
     properties = {}
-    with open(file_path, 'r') as file:
+    with open(filepath, 'r') as file:
         for line in file:
-            line = line.strip()
-            if line and not line.startswith('#'):
-                key, value = line.split('=', 1)
+            if '=' in line:
+                key, value = line.strip().split('=', 1)
                 properties[key.strip()] = value.strip()
     return properties
-
-def load_xml_files(directory_path):
-    xml_files = {}
-    for file_name in os.listdir(directory_path):
-        if file_name.endswith('.xml'):
-            xml_files[file_name] = os.path.join(directory_path, file_name)
-    return xml_files
