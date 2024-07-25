dfe222099ee3555462da4266b75f43d063879680
kavyasrini24 <103719161+kavyasrini24@users.noreply.github.com>
2024-07-25 18:09:02 +0530

    Update Model.py

--- Model.py
+++ Model.py
@@ -1,24 +1,14 @@
-import os
-from xml.etree import ElementTree as ET
+import xml.etree.ElementTree as ET
 
 class Model:
-    def __init__(self, properties_file, xml_files_directory):
-        self.properties = self.load_properties(properties_file)
-        self.xml_files = self.load_xml_files(xml_files_directory)
-    
-    def load_properties(self, file_path):
-        properties = {}
-        with open(file_path, 'r') as file:
-            for line in file:
-                if '=' in line and line.strip() and not line.startswith('#'):
-                    key, value = line.strip().split('=', 1)
-                    properties[key] = value
-        return properties
-    
-    def load_xml_files(self, directory):
+    def __init__(self, application_properties_file, xml_files_directory):
+        self.application_properties_file = application_properties_file
+        self.xml_files = self._load_xml_files(xml_files_directory)
+
+    def _load_xml_files(self, directory_path):
         xml_files = {}
-        for file_name in os.listdir(directory):
+        for file_name in os.listdir(directory_path):
             if file_name.endswith('.xml'):
-                file_path = os.path.join(directory, file_name)
-                xml_files[file_name] = ET.parse(file_path).getroot()
+                tree = ET.parse(os.path.join(directory_path, file_name))
+                xml_files[file_name] = tree.getroot()
         return xml_files
