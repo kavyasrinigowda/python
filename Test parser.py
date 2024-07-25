86d75242b75e1c512de84d134f36b11eb6e32fbd
kavyasrini24 <103719161+kavyasrini24@users.noreply.github.com>
2024-07-25 13:43:37 +0530

    Update Test parser.py

--- Test parser.py
+++ Test parser.py
@@ -2,9 +2,15 @@ import unittest
 from src.parser import parse_properties_file
 
 class TestParser(unittest.TestCase):
+    def setUp(self):
+        # Define properties for testing
+        self.properties = {
+            'key1': '${value1',
+            'key2': 'value2'
+        }
+
     def test_parse_properties_file(self):
-        properties = {'key1': '${value1', 'key2': 'value2'}
-        problems = parse_properties_file(properties)
+        problems = parse_properties_file(self.properties)
         self.assertGreater(len(problems), 0)
 
 if __name__ == '__main__':
